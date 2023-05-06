e10d91c7d550431a598e1eb61541bee9a698072f
Thais Rodrigues <71556315+thaisNY@users.noreply.github.com>
2023-05-06 16:33:35 -0300

    final
    final

--- aula5/app.js
+++ aula5/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.use(express.static('public'));
 
 app.get('/', function(req,res){
-    res.send("Pagina Home");
+    res.render('home');
 })
 app.get('/sobre', function(req,res){
-    res.send("Quem somos");
+    res.render('sobre');
 })
 
 app.listen(3000, function(){
